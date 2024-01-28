ae85d9fcb4ec7d083694c12830ef4bc1bb24fc85
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-28 15:36:44 +0800

    Initial commit

---
+++ src/main/java/ru/otus/basic/java/homework/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.basic.java.homework;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ src/main/java/ru/otus/basic/java/homework/PingServer.java
@@ -0,0 +1,12 @@
+package ru.otus.basic.java.homework;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
+public class PingServer {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
