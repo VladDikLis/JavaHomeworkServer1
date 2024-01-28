762808d0990af5034ae03c736dede94b336cb21c
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-01-28 15:37:40 +0800

    homework

--- src/main/java/ru/otus/basic/java/homework/Main.java
+++ src/main/java/ru/otus/basic/java/homework/Main.java
@@ -6,6 +6,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8088);
+        Scanner scanner = new Scanner(System.in);
 
+        String userInput1 = "8";
+        String userInput2 = "+";
+        String userInput3 = "8";
+        String userInput = userInput1 + ";" + userInput2 + ";" + userInput3 + ";";
+
+        socket.getOutputStream().write(userInput.getBytes());
+        socket.getOutputStream().flush();
+
+        byte [] request = new byte[100];
+        socket.getInputStream().read(request);
+        String fullRequest = new String(request);
+        String[] parts = fullRequest.split(";");
+
+        System.out.println(parts[0]);
     }
 }
